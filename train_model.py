b9055a02af321369ea1f011ac93d4e7bcabc585a
KnuteLute <knut.selstad@gmail.com>
2023-09-18 12:24:38 +0200

    stach

--- train_model.py
+++ train_model.py
@@ -1,6 +1,6 @@
 ASK=1
 KNUT=2
-KRISTOFFER=4
+JET=4
 SIGURD=3
 
 import os
@@ -14,22 +14,33 @@ def load_assets(path):
         raise Exception('expected different folder')
 
     dataset = {}
-    for folder, id in zip(folders, [ASK, KNUT, KRISTOFFER, SIGURD]):
+    for folder, id in zip(folders, [ASK, KNUT, JET, SIGURD]):
         for filepath in os.listdir(os.path.join(path, folder)):
             dataset.insert(id, filepath)
 
     print(dataset)
     return dataset
 
-def train_custom_model(dataset):
+def train_embedding_img(img, model):
 
-    model = cv2.dnn.readNetFromTorch('path_to_model.t7')
+
 
     img = cv2.imread('image.jpg')
     blob = cv2.dnn.blobFromImage(img, 1.0, (300, 300), (104, 117, 123), swapRB=True)
 
     model.setInput(blob)
     embeddings = model.forward()
+
+
+def train_embedding_person(folder, model, id):
+
+    for img in folder:
+        train_embedding_img(img, model)
+
+
+#def get_final_embeddings(id):
+
+
 
 if __name__ == '__main__':
 
@@ -40,5 +51,15 @@ if __name__ == '__main__':
     else:        
         path = sys.argv[1]
 
-    load_assets(path)
-    
+    datasetfiles = load_assets(path)
+
+    model = cv2.dnn.readNetFromTorch('models.t7')
+
+    train_embedding_person(datasetfiles[ASK], model, ASK)
+    train_embedding_person(datasetfiles[KNUT], model, KNUT)
+    train_embedding_person(datasetfiles[JET], model, JET)
+    train_embedding_person(datasetfiles[SIGURD], model, SIGURD)
+
+
+
+
